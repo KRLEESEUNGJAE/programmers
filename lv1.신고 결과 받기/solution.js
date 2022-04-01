a713c6e782f6ee66cfd09b522fec14e6a7760f61
KRLEESEUNGJAE <karmaup0711@gmail.com>
2022-04-01 22:44:31 +0900

    trying...

---
+++ lv1.신고 결과 받기/solution.js
@@ -0,0 +1,10 @@
+console.log('hello');
+const id_list = ['muzi', 'frodo', 'apeach', 'neo'];
+const report = ['muzi frodo', 'apeach frodo', 'frodo neo', 'muzi neo', 'apeach muzi'];
+const k = 2;
+
+for (let i = 0; i < report.length; i++) {
+  const temp = report[i].split(' ');
+  console.log(temp);
+  temp[1];
+}
